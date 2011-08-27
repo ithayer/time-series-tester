9f662ec6ec16425bb8020abcbe2f5273f28559a2
Ignacio Thayer <ignacio@albacore.(none)>
2011-08-27 10:26:43 -0700

    Simple fake data gen.

--- clojure/src/time_series_tester/core.clj
+++ clojure/src/time_series_tester/core.clj
@@ -1,6 +1,11 @@
 (ns time-series-tester.core
   (:import [hyphotesisTest TimePoint]))
 
+(defn run-fake-test
+  "Generates some random timepoints and data"
+  [n-points max-magnitude]
+  (map (fn [i] [i (rand-int max-magnitude)]) (range n-points)));; TODO: When time point constructors are done, create timepoints.
+
 (defn -main [& args]
   (let [t (TimePoint.)]
     (println "Clojure main: " (str t))))
