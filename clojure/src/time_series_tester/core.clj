c7f6c32f4cf93128b38fad7edade0d7d32848582
Ignacio Thayer <ignacio@albacore.(none)>
2011-08-27 10:08:30 -0700

    Checkin clj->java test.

--- clojure/src/time_series_tester/core.clj
+++ clojure/src/time_series_tester/core.clj
@@ -1,4 +1,6 @@
-(ns time-series-tester.core)
+(ns time-series-tester.core
+  (:import [hyphotesisTest TimePoint]))
 
 (defn -main [& args]
-  (println "Clojure main."))
+  (let [t (TimePoint.)]
+    (println "Clojure main: " (str t))))
