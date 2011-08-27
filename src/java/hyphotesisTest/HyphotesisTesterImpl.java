f9d56c4994d148839a55e40c11cec0227832abe7
alessandro magnani <ale.magnani@gmail.com>
2011-08-27 10:27:35 -0700

    added fakeSeries and empy implementation

---
+++ src/java/hyphotesisTest/HyphotesisTesterImpl.java
@@ -0,0 +1,13 @@
+package hyphotesisTest;
+
+import java.util.List;
+
+public class HyphotesisTesterImpl implements HyphotesisTester {
+
+	@Override
+	public HyphotesisResult test(List<TimePoint> series, int timeIndexTransition) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
